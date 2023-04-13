837035956224748c7161971575b0186c9c7981d2
cabrini <cabrinireis@gmail.com>
2023-04-12 23:38:28 -0300

    chore: add config theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,19 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#fa7268",
+        secondary: "#f8f9fd",
+        accent: "#82B1FF",
+        error: "#fa7268",
+        warning: "#FFDE03",
+        success: "#4caf50",
+        info: "#2196f3",
+        textDark: "#fff",
+      },
+    },
+  },
+});
